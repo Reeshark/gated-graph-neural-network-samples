174d8dd6475a9aa3eee9bd1bbb5b8d6dce7a2aa2
thaonguyen19 <thachthao2605@gmail.com>
2018-03-13 02:51:37 -0700

    added pytorch functionalities

---
+++ chem_pytorch.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env/python
+
+import torch
+import time
+import os
+import json
+import numpy as np
+import pickle
+import random
+from utils_pytorch import MLP, ThreadedIterator, AverageMeter, SMALL_NUMBER
+#from callbacks import EarlyStopping
+from torch.autograd import Variable
+from model_pytorch import repackage_hidden, GGNN
+
+def graph_to_adj_mat(graph, max_n_vertices, num_edge_types, tie_fwd_bkwd=True):
+    bwd_edge_offset = 0 if tie_fwd_bkwd else (num_edge_types // 2)
+    amat = np.zeros((num_edge_types, max_n_vertices, max_n_vertices))
+    for src, e, dest in graph:
+        amat[e-1, dest, src] = 1
+        amat[e-1 + bwd_edge_offset, src, dest] = 1
+    return amat
+
+
+class ChemModel(object):
+
+    def __init__(self, data_dir, config_file=None):
+        self.data_dir = data_dir
+
+        self.run_id = "_".join([time.strftime("%Y-%m-%d-%H-%M-%S"), str(os.getpid())])
+        log_dir = '.'
+        self.log_file = os.path.join(log_dir, "%s_log.json" % self.run_id)
+        self.best_model_file = os.path.join(log_dir, "%s_model_best.pickle" % self.run_id)
+
+        # Collect parameters:
+        self.params = {
+            'num_epochs': 3000,
+            'patience': 25,
+            'learning_rate': 0.0001,
+            'clamp_gradient_norm': 1.0,
+            'out_layer_dropout_keep_prob': 1.0,
+
+            'hidden_size': 100,
+            'num_timesteps': 4,
+            'use_graph': True,
+
+            'tie_fwd_bkwd': True,
+            'task_ids': [0],
+
+            'random_seed': 0,
+            'use_cuda': False
+        }
+        if config_file is not None:
+            with open(config_file, 'r') as f:
+                self.params.update(json.load(f))
+        with open(os.path.join(log_dir, "%s_params.json" % self.run_id), "w") as f:
+            json.dump(self.params, f)
+        print("Run %s starting with following parameters:\n%s" % (self.run_id, json.dumps(self.params)))
+        random.seed(self.params['random_seed'])
+        np.random.seed(self.params['random_seed'])
+
+        # Load data:
+        self.max_num_vertices = 0
+        self.num_edge_types = 0
+        self.annotation_size = 0
+        #self.train_data = self.load_data("molecules_train.json", is_training_data=True)
+        #self.valid_data = self.load_data("molecules_valid.json", is_training_data=False)
+
+        # Build the actual model
+        torch.manual_seed(self.params['random_seed'])
+        if self.params['use_cuda']:
+            torch.cuda.manual_seed_all(self.params['random_seed'])
+
+
+    def load_data(self, file_name, is_training_data):
+        full_path = os.path.join(self.data_dir, file_name)
+
+        print("Loading data from %s" % full_path)
+        with open(full_path, 'r') as f:
+            data = json.load(f)
+
+        #restrict = self.args.get("--restrict_data")
+        #if restrict is not None and restrict > 0:
+        #    data = data[:restrict]
+
+        # Get some common data out:
+        num_fwd_edge_types = 0
+        for g in data:
+            self.max_num_vertices = max(self.max_num_vertices, max([v+1 for e in g['graph'] for v in [e[0], e[2]]]))
+            num_fwd_edge_types = max(num_fwd_edge_types, max([e[1] for e in g['graph']]))
+        self.num_edge_types = max(self.num_edge_types, num_fwd_edge_types * (1 if self.params['tie_fwd_bkwd'] else 2)) ###directed graph
+        self.annotation_size = max(self.annotation_size, len(data[0]["node_features"][0])) ###5
+        return self.process_raw_graphs(data, self.num_edge_types, is_training_data)
+
+    @staticmethod
+    def graph_string_to_array(graph_string): #return List[List[int]]
+        return [[int(v) for v in s.split(' ')]
+                for s in graph_string.split('\n')]
+
+    def process_raw_graphs(self, raw_data, is_training_data):
+        raise Exception("Models have to implement process_raw_graphs!")
+
+    def make_model(self):
+        return GGNN(self.params['hidden_size'], self.annotation_size, self.num_edge_types, self.max_num_vertices, self.params['num_timesteps'])
+        # self.placeholders['target_values'] = tf.placeholder(tf.float32, [len(self.params['task_ids']), None],
+        #                                                     name='target_values')
+        # self.placeholders['target_mask'] = tf.placeholder(tf.float32, [len(self.params['task_ids']), None],
+        #                                                   name='target_mask')
+        # self.placeholders['num_graphs'] = tf.placeholder(tf.int64, [], name='num_graphs')
+        # self.placeholders['out_layer_dropout_keep_prob'] = tf.placeholder(tf.float32, [], name='out_layer_dropout_keep_prob')
+
+        # with tf.variable_scope("graph_model"):
+        #     self.prepare_specific_graph_model()
+        #     # This does the actual graph work:
+        #     if self.params['use_graph']:
+        #         self.ops['final_node_representations'] = self.compute_final_node_representations()
+        #     else:
+        #         self.ops['final_node_representations'] = tf.zeros_like(self.placeholders['initial_node_representation'])
+
+        # self.ops['losses'] = []
+        # for (internal_id, task_id) in enumerate(self.params['task_ids']):
+        #     with tf.variable_scope("out_layer_task%i" % task_id):
+        #         with tf.variable_scope("regression_gate"):
+        #             self.weights['regression_gate_task%i' % task_id] = MLP(2 * self.params['hidden_size'], 1, [],
+        #                                                                    self.placeholders['out_layer_dropout_keep_prob'])
+        #         with tf.variable_scope("regression"):
+        #             self.weights['regression_transform_task%i' % task_id] = MLP(self.params['hidden_size'], 1, [],
+        #                                                                         self.placeholders['out_layer_dropout_keep_prob'])
+        #         computed_values = self.gated_regression(self.ops['final_node_representations'],
+        #                                                 self.weights['regression_gate_task%i' % task_id],
+        #                                                 self.weights['regression_transform_task%i' % task_id])
+        #         diff = computed_values - self.placeholders['target_values'][internal_id,:]
+        #         task_target_mask = self.placeholders['target_mask'][internal_id,:]
+        #         task_target_num = tf.reduce_sum(task_target_mask) + SMALL_NUMBER
+        #         diff = diff * task_target_mask  # Mask out unused values
+        #         self.ops['accuracy_task%i' % task_id] = tf.reduce_sum(tf.abs(diff)) / task_target_num
+        #         task_loss = tf.reduce_sum(0.5 * tf.square(diff)) / task_target_num
+        #         # Normalise loss to account for fewer task-specific examples in batch:
+        #         task_loss = task_loss * (1.0 / (self.params['task_sample_ratios'].get(task_id) or 1.0))
+        #         self.ops['losses'].append(task_loss)
+        # self.ops['loss'] = tf.reduce_sum(self.ops['losses'])
+
+
+    def gated_regression(self, last_h, regression_gate, regression_transform):
+        raise Exception("Models have to implement gated_regression!")
+
+    def prepare_specific_graph_model(self):
+        raise Exception("Models have to implement prepare_specific_graph_model!")
+
+    def compute_final_node_representations(self):
+        raise Exception("Models have to implement compute_final_node_representations!")
+
+    def make_minibatch_iterator(self, data, is_training):
+        raise Exception("Models have to implement make_minibatch_iterator!")
+
+    def run_epoch(self, loader, criterion, optimizer, is_training):
+        chemical_accuracies = np.array([0.066513725, 0.012235489, 0.071939046, 0.033730778, 0.033486113, 0.004278493,
+                                        0.001330901, 0.004165489, 0.004128926, 0.00409976, 0.004527465, 0.012292586,
+                                        0.037467458])
+        if is_training:
+            self.model.train()
+        else:
+            self.model.eval()
+        total_loss = 0
+        #accuracies = []
+        n_processed_data= 0
+        batch_iterator = ThreadedIterator(loader, max_queue_size=2*self.params['batch_size'])
+        for step, batch_data in enumerate(batch_iterator):
+            adj_matrix, annotation, padded_annotation, target = batch_data
+            #padding = np.zeros((annotation.shape[0], self.max_num_vertices, self.params['hidden_size'] - self.annotation_size))#.double()
+            #init_input = np.concatenate((annotation, padding), axis=2) # batch_size x n_nodes x hidden_size
+            batch_size = target.size()[0]
+            n_processed_data += batch_size
+            init_input = padded_annotation
+            init_input = Variable(init_input, requires_grad=False)
+            adj_matrix = Variable(adj_matrix, requires_grad=False)
+            annotation = Variable(annotation, requires_grad=False)
+            target = Variable(target, requires_grad=False)
+
+            if self.params['use_cuda']:
+                init_input = init_input.cuda()
+                adj_matrix = adj_matrix.cuda()
+                annotation = annotation.cuda()
+                target = target.cuda()
+                criterion = criterion.cuda()
+
+            optimizer.zero_grad()
+            self.model.zero_grad()
+            output = self.model(init_input, annotation, adj_matrix)
+            loss = criterion(output, target) #* num_graphs
+            total_loss += loss.cpu().data.numpy()
+            #acc = (output.cpu().data.numpy() == target.cpu().data.numpy()).sum() ################ CHECK
+            #accuracies.append(acc) 
+
+            # for m in self.model.parameters():
+            #     print (m)
+            # print(self.model)
+            if is_training:
+                loss.backward(retain_variables=True)
+                torch.nn.utils.clip_grad_norm(self.model.parameters(), self.params['clamp_gradient_norm'])
+                optimizer.step()
+
+        #accuracies = np.sum(accuracies, axis=0)/ n_processed_data
+        #loss = loss / processed_graphs
+        return np.asscalar(total_loss[0])#, accuracies
+
+
+    def train(self):
+        val_data = self.load_data('molecules_valid.json', False)
+        train_data = self.load_data('molecules_train.json', True)
+        self.model = self.make_model()
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.params['learning_rate'])
+        
+        train_loader = self.make_minibatch_iterator(train_data, True)
+        val_loader = self.make_minibatch_iterator(val_data, False)
+        if self.params['use_cuda']:
+            self.model.cuda()
+        #es = EarlyStopping('loss', min_delta=0, patience=self.params['patience'])
+        log_to_save = []
+        best_val_loss, best_val_loss_epoch = float("inf"), 0
+        for epoch in range(self.params['num_epochs']):
+            print("epoch {}".format(epoch))         
+            train_loss = self.run_epoch(train_loader, criterion, optimizer, True)
+            print("Epoch {} Train loss {}".format(epoch, train_loss))
+            val_loss = self.run_epoch(val_loader, criterion, optimizer, False)
+            print("Epoch {} Val loss {}".format(epoch, val_loss))
+
+            log_entry = {
+                        'epoch': epoch,
+                        'train_results': (train_loss),
+                        'valid_results': (val_loss),
+                        }
+            log_to_save.append(log_entry)
+            with open(self.log_file, 'w') as f:
+                for i in range(len(log_to_save)):
+                    json.dump(log_to_save[i], f, indent=4)
+
+            if val_loss < best_val_loss:
+                self.save_model(self.best_model_file)
+                print(" (Best epoch so far, cum. val. loss decreased to %.5f from %.5f. Saving to '%s')" % (val_loss, best_val_loss, self.best_model_file))
+                best_val_loss = val_loss
+                best_val_loss_epoch = epoch
+            elif epoch - best_val_loss_epoch >= self.params['patience']:
+                print("Stopping training after %i epochs without improvement on validation loss." % self.params['patience'])
+                break
+ 
+
+    def save_model(self, path):
+        data_to_save = {
+                         "params": self.params,
+                         "model_weights": self.model.state_dict()
+                       }
+        torch.save(data_to_save, path)
+
+    def restore_model(self, path):
+        pass
---
+++ chem_pytorch_sparse.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env/python
+"""
+Usage:
+    chem_tensorflow_sparse.py [options]
+
+Options:
+    -h --help                Show this screen.
+    --config-file FILE       Hyperparameter configuration file path (in JSON format).
+    --config CONFIG          Hyperparameter configuration dictionary (in JSON format).
+    --log_dir DIR            Log dir name.
+    --data_dir DIR           Data dir name.
+    --restore FILE           File to restore weights from.
+    --freeze-graph-model     Freeze weights of graph model components.
+"""
+from collections import defaultdict
+import numpy as np
+import torch
+import sys, traceback
+#import tensorflow as tf
+from chem_pytorch import ChemModel
+from utils_pytorch import glorot_init, SMALL_NUMBER
+
+def create_adjacency_matrix(edges, n_nodes, n_edge_types):
+    a = np.zeros([n_nodes, n_nodes * n_edge_types * 2])
+    for edge in edges:
+        src_idx = edge[0]
+        e_type = edge[1]
+        tgt_idx = edge[2]
+        a[tgt_idx-1][(e_type - 1) * n_nodes + src_idx - 1] =  1
+        a[src_idx-1][(e_type - 1 + n_edge_types) * n_nodes + tgt_idx - 1] =  1
+    return a
+
+
+class SparseGGNNChemModel(ChemModel):
+    def __init__(self, args):
+        super(SparseGGNNChemModel, self).__init__(args)
+        self.params.update({
+            'batch_size': 20,
+            'use_edge_bias': False,
+            'use_edge_msg_avg_aggregation': True,
+
+            'layer_timesteps': [1, 1, 1, 1],  # number of layers & propagation steps per layer
+
+            'graph_rnn_cell': 'GRU',  # GRU or RNN
+            'graph_rnn_activation': 'tanh',  # tanh, ReLU
+            'graph_state_dropout_keep_prob': 1.,
+            'task_sample_ratios': {},
+        })
+
+    def compute_final_node_representations(self):
+        pass
+        # cur_node_states = self.placeholders['initial_node_representation']  # number of nodes in batch v x D
+        # num_nodes = tf.shape(self.placeholders['initial_node_representation'], out_type=tf.int64)[0]
+
+        # for (layer_idx, num_timesteps) in enumerate(self.params['layer_timesteps']):
+        #     with tf.variable_scope('gnn_layer_%i' % layer_idx):
+        #         adjacency_matrices = []  # type: List[tf.SparseTensor]
+        #         for adjacency_list_for_edge_type in self.placeholders['adjacency_lists']:
+        #             # adjacency_list_for_edge_type (shape [-1, 2]) includes all edges of type e_type of a sparse graph with v nodes (ids from 0 to v).
+        #             adjacency_matrix_for_edge_type = tf.SparseTensor(indices=adjacency_list_for_edge_type,
+        #                                                              values=tf.ones_like(
+        #                                                                  adjacency_list_for_edge_type[:, 1],
+        #                                                                  dtype=tf.float32),
+        #                                                              dense_shape=[num_nodes, num_nodes])
+        #             adjacency_matrices.append(adjacency_matrix_for_edge_type)
+
+        #         for step in range(num_timesteps):
+        #             with tf.variable_scope('timestep_%i' % step):
+        #                 incoming_messages = []  # list of v x D
+
+        #                 # Collect incoming messages per edge type
+        #                 for adjacency_matrix in adjacency_matrices:
+        #                     incoming_messages_per_type = tf.sparse_tensor_dense_matmul(adjacency_matrix,
+        #                                                                                cur_node_states)  # v x D
+        #                     incoming_messages.extend([incoming_messages_per_type])
+
+        #                 # Pass incoming messages through linear layer:
+        #                 incoming_messages = tf.concat(incoming_messages, axis=1)  # v x [2 *] edge_types
+        #                 messages_passed = tf.matmul(incoming_messages,
+        #                                             self.weights['edge_weights'][layer_idx])  # v x D
+
+        #                 if self.params['use_edge_bias']:
+        #                     messages_passed += tf.matmul(self.placeholders['num_incoming_edges_per_type'],
+        #                                                  self.weights['edge_biases'][layer_idx])  # v x D
+
+        #                 if self.params['use_edge_msg_avg_aggregation']:
+        #                     num_incoming_edges = tf.reduce_sum(self.placeholders['num_incoming_edges_per_type'],
+        #                                                        keep_dims=True, axis=-1)  # v x 1
+        #                     messages_passed /= num_incoming_edges + SMALL_NUMBER
+
+        #                 # pass updated vertex features into RNN cell
+        #                 cur_node_states = self.weights['rnn_cells'][layer_idx](messages_passed, cur_node_states)[1]  # v x D
+
+        # return cur_node_states
+
+    def gated_regression(self, last_h, regression_gate, regression_transform):
+        pass
+        # # last_h: [v x h]
+        # gate_input = tf.concat([last_h, self.placeholders['initial_node_representation']], axis=-1)  # [v x 2h]
+        # gated_outputs = tf.nn.sigmoid(regression_gate(gate_input)) * regression_transform(last_h)  # [v x 1]
+
+        # # Sum up all nodes per-graph
+        # num_nodes = tf.shape(gate_input, out_type=tf.int64)[0]
+        # graph_nodes = tf.SparseTensor(indices=self.placeholders['graph_nodes_list'],
+        #                               values=tf.ones_like(self.placeholders['graph_nodes_list'][:, 0],
+        #                                                   dtype=tf.float32),
+        #                               dense_shape=[self.placeholders['num_graphs'], num_nodes])  # [g x v]
+        # return tf.squeeze(tf.sparse_tensor_dense_matmul(graph_nodes, gated_outputs), axis=[-1])  # [g]
+
+    # ----- Data preprocessing and chunking into minibatches:
+    def process_raw_graphs(self, raw_data, num_edge_types, is_training_data):
+        processed_graphs = []
+        for d in raw_data:
+            processed_graphs.append({"edge_list": d['graph'],
+                                     "annotation": d["node_features"],
+                                     "label": [d["targets"][task_id][0] for task_id in self.params['task_ids']]})
+        np.random.shuffle(processed_graphs)
+
+        # if is_training_data:
+        #     np.random.shuffle(processed_graphs)
+        #     for task_id in self.params['task_ids']:
+        #         task_sample_ratio = self.params['task_sample_ratios'].get(str(task_id))
+        #         if task_sample_ratio is not None:
+        #             ex_to_sample = int(len(processed_graphs) * task_sample_ratio)
+        #             for ex_id in range(ex_to_sample, len(processed_graphs)):
+        #                 processed_graphs[ex_id]['labels'][task_id] = None
+
+        return processed_graphs
+
+    def __graph_to_adjacency_lists(self, graph, num_edge_types): 
+        '''
+        return Tuple of Dict[int, np.ndarray] and Dict[int, Dict[int, int]]]
+        adj_list: {edge type: list of (src, dest)}
+        num_incoming_edges_dicts_per_type: {edge type: {node_id: num_incoming_edges}}
+        '''
+        adj_lists = defaultdict(list)
+        num_incoming_edges_dicts_per_type = defaultdict(lambda: defaultdict(lambda: 0))
+        for src, e, dest in graph:
+            fwd_edge_type = e - 1  # Make edges start from 0
+            adj_lists[fwd_edge_type].append((src, dest))
+            num_incoming_edges_dicts_per_type[fwd_edge_type][dest] += 1
+            if self.params['tie_fwd_bkwd']:
+                adj_lists[fwd_edge_type].append((dest, src))
+                num_incoming_edges_dicts_per_type[fwd_edge_type][src] += 1
+
+        final_adj_lists = {e: np.array(sorted(lm), dtype=np.int32)
+                           for e, lm in adj_lists.items()}
+
+        # Add backward edges as an additional edge type that goes backwards:
+        if not (self.params['tie_fwd_bkwd']):
+            for (edge_type, edges) in adj_lists.items():
+                bwd_edge_type = num_edge_types + edge_type
+                final_adj_lists[bwd_edge_type] = np.array(sorted((y, x) for (x, y) in edges), dtype=np.int32)
+                for (x, y) in edges:
+                    num_incoming_edges_dicts_per_type[bwd_edge_type][y] += 1
+
+        return final_adj_lists, num_incoming_edges_dicts_per_type
+
+    #def make_minibatch_iterator(self, data, is_training):
+    #    if is_training:
+    #        np.random.shuffle(data)
+    #    self.dataloader = get_chem_dataloader(data, batch_size=self.params['batch_size'])
+
+
+    def make_minibatch_iterator(self, data, is_training):
+        """Create minibatches by flattening adjacency matrices into a single adjacency matrix with
+        multiple disconnected components.
+        Batch size = total number of nodes from each sample
+        RETURN: tuple of
+        np.array(batch_node_features),
+        np.concatenate(batch_num_incoming_edges_per_type, axis=0),
+        np.array(batch_graph_nodes_list, dtype=np.int32),
+        np.transpose(batch_target_task_values, axes=[1,0]),
+        #np.transpose(batch_target_task_mask, axes=[1, 0]),
+        #num_graphs_in_batch,
+        #dropout_keep_prob,
+        adj_list
+
+        initial_node_representation
+        num_incoming_edges_per_type
+        graph_nodes_list
+        target_values
+        target_mask 
+        num_graphs 
+        graph_state_keep_prob
+
+        """
+
+        # Pack until we cannot fit more graphs in the batch
+        num_graphs = 0
+        while num_graphs < len(data):
+            num_graphs_in_batch = 0
+            batch_node_features_padded = []
+            batch_node_features = []
+            batch_target_task_values = []
+            batch_adj_matrix = []
+            while num_graphs < len(data) and num_graphs_in_batch < self.params['batch_size']:
+                cur_graph = data[num_graphs]
+                num_graphs_in_batch += 1
+                num_nodes_in_graph = len(cur_graph['annotation'])
+                padded_features = np.pad(cur_graph['annotation'],
+                                         ((0, 0), (0, self.params['hidden_size'] - self.annotation_size)),
+                                         'constant') #(len of node_features (i.e. annotation_size), hidden_size)
+                node_feature = np.zeros((self.max_num_vertices, self.params['hidden_size']))
+                for j in range(num_nodes_in_graph):
+                    for src, _, dest in cur_graph['edge_list']:
+                        node_feature[src, :] += padded_features[j, :]
+                        node_feature[dest, :] += padded_features[j, :]
+                batch_node_features_padded.append(node_feature)#padded_features)
+                adj_matrix = create_adjacency_matrix(cur_graph['edge_list'], self.max_num_vertices, self.num_edge_types)
+                batch_adj_matrix.append(adj_matrix)
+
+                target_task_values = []
+                for target_val in cur_graph['label']:
+                    target_task_values.append(target_val)
+                batch_target_task_values.append(target_task_values)
+
+            num_graphs += num_graphs_in_batch
+            batch_data = (  
+                            torch.FloatTensor(np.array(batch_adj_matrix).astype(float)),
+                            torch.FloatTensor(np.copy(np.array(batch_node_features_padded))[:, :, :self.annotation_size]),
+                            torch.FloatTensor(np.array(batch_node_features_padded)),
+                            torch.FloatTensor(np.array(batch_target_task_values)) #axes=[1,0]
+                        )
+            yield batch_data
+
+
+def main():
+    try:
+        model = SparseGGNNChemModel('./')
+        model.train()
+    except:
+        typ, value, tb = sys.exc_info()
+        traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
